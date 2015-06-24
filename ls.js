0d7bb462c588c81bd4ef8463349a40d7469252c7
Tkachenko Sergey <ts1503@yandex-team.ru>
2015-06-24 19:32:15 +0300

    порефакторил

--- ls.js
+++ ls.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var Response = require('Response');
+var EventEmitter = require('EventEmitter');
 
 var KEY_REGEXP = /^change:(.*)$/;
 
 var ls = {};
+var emitter;
 var storage = checkAndGetStorage();
-var addListener = getListenerFunc();
+var addListener;
 
 module.exports = ls;
 
@@ -17,6 +19,13 @@ ls.isSupported = function () {
 if (!storage) {
     return;
 }
+
+emitter = new EventEmitter();
+addListener = getListenerFunc();
+
+addListener('storage', function (e) {
+    emitter.trigger(e.key, e.newValue);
+}, false);
 
 ls.set = function (key, value, strict) {
     var r = new Response();
@@ -60,21 +69,25 @@ ls.getValue = function (key, type) {
 ls.on = function (key, listener, context) {
     var name = getKeyName(key);
 
-    addListener('storage', function(e) {
-        if (e.key === name && e.oldValue !== e.newValue) {
-            listener.call(context, e.newValue);
-        }
-    }, false);
+    emitter.on(name, listener, context);
 
     return this;
 };
 
 ls.once = function (key, listener, context) {
+    var name = getKeyName(key);
 
+    emitter.once(name, listener, context);
+
+    return this;
 };
 
 ls.off = function (key, listener, context) {
+    var name = getKeyName(key);
 
+    emitter.off(name, listener, context);
+
+    return this;
 };
 
 function checkAndGetStorage() {
@@ -98,6 +111,7 @@ function getListenerFunc() {
         return document.body.addEventListener.bind(document.body);
     }
 }
+
 function stringifyValue(value) {
     var result;
 
